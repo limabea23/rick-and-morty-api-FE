837cd275c927f492c339b221ead2c6130ab66cff
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2025-04-17 11:39:25 -0300

    feat: filter and toast

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -2,32 +2,74 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import styles from "../home/Home.module.css";
 
 import CharacterCard from "../../components/CharacterCard";
 
 export default function Home() {
+    const [search, setSearch] = useState("");
+    const [notFound, setNotFound] = useState(false);
     const [characters, setCharacters] = useState([]);
 
+    const fetchCharacters = async (name = "") => {
+        setNotFound(false);
+        try {
+            const { data } = await axios.get(
+                `https://rickandmortyapi.com/api/character/?name=${name}`
+            );
+            setCharacters(data.results);
+        } catch {
+            setCharacters([]);
+            setNotFound(true);
+        }
+    };
+    
     useEffect(() => {
-        axios
-        .get('https://rickandmortyapi.com/api/character')
-        .then((response) => {
-            setCharacters(response.data.results)
-        })
-        .catch((error) => {
-            console.log("Erro ao buscar personagens:", error);
-        });
-    }), [];
+        fetchCharacters();
+    }, []);
 
-    console.log(characters);
+    const handleCardClick = (name) => {
+        toast.info(`Você clicou em ${name}`, {
+        });
+    }
 
     return (
         <div className={styles.container}>
+            <ToastContainer position="top-right" autoClose={7500} theme="light" />
+            <h1 className={styles.title}>Rick and Morty Characters</h1>
+            <div className={styles.controls}>
+        <input
+          type="text"
+          placeholder="Buscar por nome"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className={styles.input}
+        />
+        <button
+          onClick={() => fetchCharacters(search.trim())}
+          className={styles.buttonSearch}
+        >
+          Buscar
+        </button>
+        <button
+          onClick={() => {
+            setSearch("");
+            fetchCharacters();
+          }}
+          className={styles.buttonReset}
+        >
+          Resetar
+        </button>
+      </div>
+            {notFound && (
+                <h1 className={styles.notFound}>AFF, nenhum personagem encontrado </h1>
+            )}
             <div className={styles.grid}>
             {characters.map((char) => (
-                <CharacterCard key={char.id} character={char} />
+                <CharacterCard key={char.id} character={char} onClick={() => handleCardClick(char.name)} />
             ))}
             </div>
         </div>
